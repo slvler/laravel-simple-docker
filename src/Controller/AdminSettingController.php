5f5567b74c1d9d4298e822f9bae3ea93e473c8e6
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2022-07-17 14:26:37 +0300

    setting section detail page

--- src/Controller/AdminSettingController.php
+++ src/Controller/AdminSettingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\AdminSetting;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +12,24 @@ class AdminSettingController extends AbstractController
 {
 
     #[Route('/admin/setting', name: 'admin.setting')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('admin/setting/index.html.twig' );
+
+        $repository = $doctrine->getRepository(AdminSetting::class);
+        $settings = $repository->findAll();
+        return $this->render('admin/setting/index.html.twig', [ 'settings' => $settings] );
     }
+
+    #[Route('admin/setting/{id<\d+>}', name: 'admin.setting.detail')]
+    public function detail(ManagerRegistry $doctrine, $id): Response
+    {
+       $repository = $doctrine->getRepository(AdminSetting::class);
+       $detail = $repository->find($id);
+       if (!$detail){
+           return $this->redirectToRoute('admin.setting');
+       }
+       return $this->render('admin/setting/edit.html.twig', [ 'setting' => $detail ]);
+    }
+
+
 }
